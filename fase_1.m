d5d3acad3dc4bcdca04b2a7ab41a9f1e0e6b5721
PolGonzalez <polgonzalezsierras@gmail.com>
2018-11-10 23:42:29 +0100

    Add files via upload

--- fase_1.m
+++ fase_1.m
@@ -1,16 +1,16 @@
 function [vb] = fase_1(A,b,m,n)
-A_1 = A;
-c_1 = zeros(1,n+m);
+c = zeros(1,n+m);
 for i = 1:m
-    c_1(n+1) = 1;
+    cA(n+1) = 1;
 end
 for i = 1:m
     new = zeros(m,1);
     new(m) = 1;
-    A_1 = [A_1 new];
+    A = [A new];
 end
 vb = n+1:n+m;
 vn = 1:n;
-xb = inv(A_1(:,vb))*b;
+abanico = A(:,vb);
+xb = inv(abanico)*b;
 z = c(:,vb)*xb; 
-[vb, vn, xb, z, iout] = simplex( c_1, A_1, b, vb, vn, xb, z,2);
+[vb, vn, xb, z, iout] = simplex( c, A, b, vb, vn, xb, z,2);
